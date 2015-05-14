2381196a48b726e9bbb8806afbf547d3d480bb24
Mitchell Thompson <leonsbuddydave@gmail.com>
2015-05-13 20:52:55 -0700

    muuuahhhh

--- src/app/models/ProductSide.model.js
+++ src/app/models/ProductSide.model.js
@@ -36,6 +36,7 @@ class ProductSide {
 					selectable: false,
 					evented: false
 				});
+				im.crossOrigin = "Anonymous";
 				im.src = this.images.shape;
 			}
 
@@ -47,6 +48,7 @@ class ProductSide {
 					selectable: false,
 					evented: false
 				});
+				im.crossOrigin = "Anonymous";
 				im.src = this.images.texture;
 			}
 		}
